0baf8c6dfac0748d05faa172d75342370b8b9c8d
Jakub Szwachła <meszkax@gmail.com>
2014-08-23 12:41:34 +0200

    Frontend: game view

--- static/app.js
+++ static/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('Shinobi', []);
+var app = angular.module('Shinobi', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider.
+        when("/games", {
+            templateUrl: "partials/games.html",
+            controller: "GamesController"
+        }).
+        when("/games/:gid", {
+            templateUrl: "partials/game.html",
+            controller: "GameController"}).
+        otherwise({
+            redirectTo: '/games'
+        });
+});
--- static/controllers.js
+++ static/controllers.js
@@ -1,13 +1,44 @@
+var root = 'http://localhost:5000';
+
 app.controller('GamesController', function ($scope, $http) {
     var update = function () {
-        $http.get('http://localhost:5000/games').success(function (data) {
-                $scope.games = data.games;
-            });
+        $http.get(root + '/games').success(function (data) {
+            $scope.games = data.games;
+        });
     };
-    
+
     $scope.createGame = function (game) {
-       $http.post('http://localhost:5000/games', game);
-       update();
+        $http.post(root + '/games', game);
+        update();
+    };
+
+    update();
+});
+
+app.controller('GameController', function ($scope, $http, $routeParams) {
+    var gid = $routeParams.gid;
+    var myPid = 1;
+    var pids;
+
+    $scope.range = function (n) {
+       var range = [];
+       for (var i = 0; i < n; i++) {
+           range.push(i);
+       }
+       return range;
+    };
+
+    var update = function () {
+        $http.get(root + '/games/' + gid).success(function (data) {
+            $scope.game = data;
+        });
+        $http.get(root + '/games/' + gid + '/players').success(function (data) {
+            $scope.players = data.players;
+        });
+        $http.get(root + '/games/' + gid + '/players/' + myPid + '/hand').
+            success(function (data) {
+                $scope.hand = data.hand;
+            });
     };
 
     update();
